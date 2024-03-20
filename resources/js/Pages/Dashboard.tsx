8a48ba0c80fc613aae6499dc27a24e7971a6b130
ryuheimasukawa <mappiinex210@gmail.com>
2024-03-20 13:01:59 +0900

    typescriptに変換

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,8 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { PageProps } from '@/types';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 
-export default function Dashboard({ auth }) {
+export default function Dashboard({ auth }: PageProps) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -12,7 +13,7 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">ログイン中</div>
+                        <div className="p-6 text-gray-900">Yi!</div>
                     </div>
                 </div>
             </div>
